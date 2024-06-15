27fbee7e229f9f3b0afc6022d444a082540d73b9
Your Name <you@example.com>
2024-06-15 11:04:07 -0500

    Prescribers v3

--- Scripts/prescribers script.sql
+++ Scripts/prescribers script.sql
@@ -156,7 +156,34 @@
 -- 7. The goal of this exercise is to generate a full list of all pain management specialists in Nashville and the number of claims they had for each opioid. **Hint:** The results from all 3 parts will have 637 rows.
 
 --     a. First, create a list of all npi/drug_name combinations for pain management specialists (specialty_description = 'Pain Management) in the city of Nashville (nppes_provider_city = 'NASHVILLE'), where the drug is an opioid (opiod_drug_flag = 'Y'). **Warning:** Double-check your query before running it. You will only need to use the prescriber and drug tables since you don't need the claims numbers yet.
-
+-- SELECT prescriber.npi
+-- 	, drug.drug_name
+-- FROM prescriber
+-- CROSS JOIN drug
+-- WHERE prescriber.specialty_description = 'Pain Management'
+-- AND prescriber.nppes_provider_city = 'NASHVILLE'
+-- AND opioid_drug_flag = 'Y'
 --     b. Next, report the number of claims per drug per prescriber. Be sure to include all combinations, whether or not the prescriber had any claims. You should report the npi, the drug name, and the number of claims (total_claim_count).
-    
+-- SELECT prescriber.npi
+-- 	, drug.drug_name
+-- 	, total_claim_count
+-- FROM prescriber
+-- CROSS JOIN drug
+-- LEFT JOIN prescription
+-- USING(drug_name, npi)
+-- WHERE prescriber.specialty_description = 'Pain Management'
+-- AND prescriber.nppes_provider_city = 'NASHVILLE'
+-- AND opioid_drug_flag = 'Y'
+-- ORDER BY total_claim_count DESC
 --     c. Finally, if you have not done so already, fill in any missing values for total_claim_count with 0. Hint - Google the COALESCE function.
+-- SELECT prescriber.npi
+-- 	, drug.drug_name
+-- 	, COALESCE(total_claim_count,0)
+-- FROM prescriber
+-- CROSS JOIN drug
+-- LEFT JOIN prescription
+-- USING(drug_name, npi)
+-- WHERE prescriber.specialty_description = 'Pain Management'
+-- AND prescriber.nppes_provider_city = 'NASHVILLE'
+-- AND opioid_drug_flag = 'Y'
+-- ORDER BY total_claim_count DESC 
